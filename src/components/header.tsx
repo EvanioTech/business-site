011423d49b3888881cb47113ff4f097d14b90f5b
Evanio Tech <evanioshark@gmail.com>
2025-11-07 18:35:03 -0300

    Refactor Header component to use CSS modules; remove old CSS file and apply new styles for improved layout and responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,29 @@
-
 import Link from "next/link";
-import './header.css';
+import Style from "./header.module.css";
 
 
 export default function Header() {
   return (
-    <header>
+    // Aplicar Style.header ao elemento <header>
+    <header className={Style.headerContainer}> 
       <nav>
-        <ul>
-          <li>
-            <Link href="/" className="text">Octadroid Tech</Link>
+        {/* Aplicar Style.ul ao <ul> */}
+        <ul className={Style.ul}>
+          <li className={Style.listItem}>
+            {/* O Style.text já está aplicado aqui */}
+            <Link href="/" className={Style.text}>Octadroid Tech</Link>
           </li>
-            <li>
-              <Link href="/web" className="text">Sistemas Web</Link>
+            <li className={Style.listItem}>
+              <Link href="/web" className={Style.text}>Sistemas Web</Link>
             </li>
-            <li>
-              <Link href="/android" className="text">Aplicativos Android</Link>
+            <li className={Style.listItem}>
+              <Link href="/android" className={Style.text}>Aplicativos Android</Link>
             </li>
-            <li>
-              <Link href="/suporte" className="text">Suporte Técnico</Link>
+            <li className={Style.listItem}>
+              <Link href="/suporte" className={Style.text}>Suporte Técnico</Link>
             </li>
         </ul>
       </nav>
     </header>
   );
 }
-
-
-
